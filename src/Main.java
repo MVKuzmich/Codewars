9bf474eda993e5e02e57d7eef074343bb9981c7c
Mikhail V. Kuzmich <kmv85@mail.ru>
2022-11-06 10:11:22 +0300

    task Find is done

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,16 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-//        int[] first = new int[]{1, 3, 4, 8, 9, 1 , 1, 3};
-//        int[] second = new int[]{1, 9};
-//        String input = "iiisdoso";
-//        System.out.println(Arrays.toString(new Tasks6().arrayDiff(first, second)));
-//        System.out.println(Arrays.toString(new Tasks6().parse(input)));
+        int[] first = new int[]{1, 3, 4, 8, 9, 1 , 1, 3};
+        int[] second = new int[]{1, 9};
+        String input = "iiisdoso";
+        System.out.println(Arrays.toString(new Tasks6().arrayDiff(first, second)));
+        System.out.println(Arrays.toString(new Tasks6().parse(input)));
 
         String str = "the-Stealth-Warrior";
         System.out.println(new Tasks6().toCamelCase(str));
+
+        int[] arr = {2, 4, 0, 100, 4, 11, 2602, 36};
+        System.out.println(new Tasks6().find(arr));
     }
 }
